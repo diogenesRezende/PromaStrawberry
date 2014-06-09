002b922566dec47764db3c6afe354bf1cc4e61bf
João Paulo Nakajima <joaopaulonakajima@gmail.com>
2014-06-09 20:46:23 -0300

    Relatorio começando

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ var Index = {
 
 	init: function(){
 		Index.localizacao();
-		Index.previsao();
+		// Index.previsao();
 	},
 	localizacao: function(){
 		var link = document.getElementById("link");
@@ -14,25 +14,25 @@ var Index = {
 			}, "jsonp");
 		});
 	},
-	previsao: function(){
-		// var aux = cidade;
-		$.get("http://ipinfo.io", function (response) {
-			cidade = response.city;
-			estado = response.region;
-		}, "jsonp");
-		// alert(cidade);
-		// var aux = cidade.replace( /\s/g, '' );
-		// var link = "http://www.tempoagora.com.br/previsao-do-tempo/brasil/" + aux + "-MG";
-		// var str = 'teste teste teste teste ';  
-		// alert( 'string ANTES do replace: '+str );
-		// alert( 'strong DEPOIS do replace: '+str.replace( /\s/g, '' ) );
-		var previsao = document.getElementById("linkPrevisao");
-		previsao.addEventListener('click',function(){
-			// http://www.tempoagora.com.br/previsao-do-tempo/brasil/Estiva-MG
-			// previsao.href = link;
+	// previsao: function(){
+	// 	// var aux = cidade;
+	// 	$.get("http://ipinfo.io", function (response) {
+	// 		cidade = response.city;
+	// 		estado = response.region;
+	// 	}, "jsonp");
+	// 	// alert(cidade);
+	// 	// var aux = cidade.replace( /\s/g, '' );
+	// 	// var link = "http://www.tempoagora.com.br/previsao-do-tempo/brasil/" + aux + "-MG";
+	// 	// var str = 'teste teste teste teste ';  
+	// 	// alert( 'string ANTES do replace: '+str );
+	// 	// alert( 'strong DEPOIS do replace: '+str.replace( /\s/g, '' ) );
+	// 	var previsao = document.getElementById("linkPrevisao");
+	// 	previsao.addEventListener('click',function(){
+	// 		// http://www.tempoagora.com.br/previsao-do-tempo/brasil/Estiva-MG
+	// 		// previsao.href = link;
 
-		});
-	}
+	// 	});
+	// }
 
 };
 Index.init();
---
+++ relatorio/js/Index.js
@@ -0,0 +1,9 @@
+var Index = {
+
+	VariedadeDAO.unserializeAndParse();
+	var listVariedade = VariedadeDAO.retrieve();
+
+	VendaDAO.unserializeAndParse();
+	var listVendas = VendaDAO.retrieve();
+	
+}
